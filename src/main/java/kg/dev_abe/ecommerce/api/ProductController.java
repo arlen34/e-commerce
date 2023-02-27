0afb732aaa62d3eff7551b5802d746608d2486bb
Erulan <era.sharsheev2001@gmail.com>
2023-02-27 18:45:51 +0600

    added pagination

--- src/main/java/kg/dev_abe/ecommerce/api/ProductController.java
+++ src/main/java/kg/dev_abe/ecommerce/api/ProductController.java
@@ -7,14 +7,15 @@ import kg.dev_abe.ecommerce.dto.request.ProductUpdateRequest;
 import kg.dev_abe.ecommerce.dto.response.ProductResponse;
 import kg.dev_abe.ecommerce.services.ProductService;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/products")
@@ -28,9 +29,12 @@ public class ProductController {
             description = "This endpoint returns a new created product with all products")
     @PreAuthorize("hasAuthority('ADMIN')")
     @PostMapping(path = "/add")
-    public List<ProductResponse> addProduct(@RequestBody ProductCreateRequest request) {
-        return productService.create(request);
+    public Page<ProductResponse> addProduct(
+            @RequestBody ProductCreateRequest request,
+            @PageableDefault(sort = {"id"},direction = Sort.Direction.DESC) Pageable pageable
+    ) {
 
+        return productService.create(request, pageable);
     }
 
     @PostMapping(path = "{id}/add-image/", consumes = {"multipart/form-data"})
@@ -42,8 +46,11 @@ public class ProductController {
     @Operation(summary = "Get all products",
             description = "This endpoint returns all products")
     @GetMapping("/get-all/{categoryId}")
-    public List<ProductResponse> getProductsByCategoryId(@PathVariable Long categoryId) {
-        return productService.getAllProductsByCategoryId(categoryId);
+    public Page<ProductResponse> getProductsByCategoryId(
+            @PathVariable Long categoryId,
+            @PageableDefault(sort = {"id"},direction = Sort.Direction.DESC,size = 2) Pageable pageable
+    ) {
+        return productService.getAllProductsByCategoryId(categoryId, pageable);
     }
 
     @Operation(summary = "Get a product by id",
@@ -66,7 +73,7 @@ public class ProductController {
             description = "This endpoint returns a deleted product with all products")
     @PreAuthorize("hasAuthority('ADMIN')")
     @DeleteMapping("/{id}")
-    public List<ProductResponse> deleteProductById(@PathVariable Long id) {
+    public Page<ProductResponse> deleteProductById(@PathVariable Long id) {
         return productService.deleteById(id);
     }
 }
--- src/main/java/kg/dev_abe/ecommerce/repositories/ProductRepository.java
+++ src/main/java/kg/dev_abe/ecommerce/repositories/ProductRepository.java
@@ -1,16 +1,19 @@
 package kg.dev_abe.ecommerce.repositories;
 
-import kg.dev_abe.ecommerce.dto.response.ProductResponse;
-import kg.dev_abe.ecommerce.dto.response.ProductResponses;
-import kg.dev_abe.ecommerce.models.Category;
 import kg.dev_abe.ecommerce.models.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import java.util.List;
-import java.util.Optional;
-
 public interface ProductRepository extends JpaRepository<Product,Long> {
     @Query("select p from Product p where p.category.id = ?1")
-    List<Product> getProductsByCategoryId(Long categoryId);
+    Page<Product> findByCategoryId(Long categoryId, Pageable pageable);
+
+    Page<Product> findByProductNameContainingIgnoreCaseOrDescriptionContainingIgnoreCase(
+            String name, String description, Pageable pageable);
+    @Override
+    Page<Product> findAll(Pageable pageable);
+
+
 }
