a1525c2013d5321a7f8f5cc43736fb9153ff79f9
arlen34 <arlen.kubanychbekovv01@gmail.com>
2023-04-22 20:01:41 +0600

    authentication with google account

--- src/main/java/kg/dev_abe/ecommerce/repositories/UserRepository.java
+++ src/main/java/kg/dev_abe/ecommerce/repositories/UserRepository.java
@@ -9,6 +9,7 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User,Long> {
     Optional<User> findByEmail(String email);
+    User findUserByEmail(String email);
 
     boolean existsUserByEmail(String email);
 
